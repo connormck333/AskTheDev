f61f245d568a74874f1bc4467a90d3d74e0656e4
Connor McKenzie <connora.mckenzie@gmail.com>
2025-03-02 17:43:38 +0000

    Web page html is provided in question and generates relevant response to current web page

---
+++ src/methods/getCurrentTabHTML.ts
@@ -0,0 +1,29 @@
+import { Status } from "../utils/interfaces";
+
+async function getCurrentTabHTML(): Promise<Status> {
+    try {
+        const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+        const response = await chrome.scripting.executeScript({
+            target: { tabId: tabs[0]?.id ? tabs[0].id : 0 },
+            func: () => document.documentElement.outerHTML
+        });
+
+        if (response[0].result === undefined) {
+            return { success: false };
+        }
+
+        return { success: true, data: removeAllTags(response[0].result) };
+    } catch (error) {
+        console.log(error);
+        return { success: false }
+    }
+}
+
+function removeAllTags(htmlContent: string): string {
+    const doc: Document = new DOMParser().parseFromString(htmlContent, "text/html");
+    return doc.body.textContent || "";
+}
+
+export {
+    getCurrentTabHTML
+}
