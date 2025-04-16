ddf690d0f565100d0775251a61415ce8a789f4be
Connor McKenzie <connora.mckenzie@gmail.com>
2025-04-16 08:46:15 +0100

    Added timestamp to user model

--- backend/AskTheDev/src/main/java/com/devconnor/askthedev/models/User.java
+++ backend/AskTheDev/src/main/java/com/devconnor/askthedev/models/User.java
@@ -3,7 +3,9 @@ package com.devconnor.askthedev.models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
@@ -20,4 +22,8 @@ public class User {
     private String password;
 
     private String customerId;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 }
